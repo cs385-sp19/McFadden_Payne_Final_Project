6546ab5aa4016aac6136a33a866850f422c20888
Adam Payne <c19adam.payne@usafa.edu>
2019-03-11 16:31:01 -0600

    test

---
+++ List_Payne/ToDoCell.swift
@@ -0,0 +1,15 @@
+import Foundation
+import UIKit
+
+@objc protocol ToDoCellDelegate: class {
+    func checkmarkTapped(sender: ToDoCell)
+}
+
+class ToDoCell: UITableViewCell {
+    @IBOutlet weak var isCompleteButton: UIButton!
+    @IBOutlet weak var titleLabel: UILabel!
+    var delegate: ToDoCellDelegate?
+    @IBAction func completeButtonTapped() {
+        delegate?.checkmarkTapped(sender: self)
+    }
+}
